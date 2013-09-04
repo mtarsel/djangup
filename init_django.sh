329f445781fb31776d7b9ae62b3d86b4b64f6375
Mick Tarsel <mtarsel@gmail.com>
2013-09-04 18:37:14 -0400

    Seperated django install and apache install. TODO: add settings.py

--- init_django.sh
+++ init_django.sh
@@ -12,7 +12,8 @@ sudo adduser $userName -m
 echo "Please enter Django project name: "
 read projectName
 
-sudo su django -c ./setup_django.sh userName=$userName projectName=$projectName #executes commands in 'setup_django.sh' for user django 
+sudo su django -c userName=$userName projectName=$projectName ./setup_django.sh 
+#executes commands in 'setup_django.sh' for user django 
 
 
 #### APACHE CONFIGS ####
@@ -31,12 +32,6 @@ Order deny,allow
 Allow from all
 </Files>
 </Directory>" > /etc/apache2/httpd.conf 
-
-echo "writiing to wsgi.py"
-
-sed -e '17s/$/sys.path.insert(0,os.sep.join(os.path.abspath(__file__).split(os.sep)[:-2]))/' -i /home/$userName/$projectName/$projectName/wsgi.py
-
-echo "written to wsgi.py"
 
 a2dissite default
 a2ensite $projectName
--- setup_django.sh
+++ setup_django.sh
@@ -16,6 +16,12 @@ mkdir css
 mkdir js
 mkdir img
 
-#TODO
+echo "writiing to wsgi.py"
+
+sed -e '17s/$/sys.path.insert(0,os.sep.join(os.path.abspath(__file__).split(os.sep)[:-2]))/' -i /home/$userName/$projectName/$projectName/wsgi.py
+
+echo "written to wsgi.py"
+
+#TODO add static file directories to settings.py
 cat /home/$userName/$projectName/$projectName/settings.py | awk '/TEMP_DIR/ {print FNR}'
 
